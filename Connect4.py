fe8ea21c929f982332b39a06a88c2dafe4153c28
Kyle-Venegas <venegas.kyle@gmail.com>
2019-07-04 11:53:02 -0700

    added methods to Board class, changed naming

--- Connect4.py
+++ Connect4.py
@@ -1,7 +1,92 @@
 GRID_WIDTH = 7
 GRID_HEIGHT = 6
 
-grid = [["_" for i in range(GRID_WIDTH)] for j in range(GRID_HEIGHT)]
+class Board:
+
+    def __init__(self):
+        self.grid = [["_" for i in range(GRID_WIDTH)] for j in range(GRID_HEIGHT)]
+
+    def draw(self):
+        print("\n  0    1    2    3    4    5    6")
+        for row in self.grid:
+            print(row)
+
+    def drop(self, col):
+        col = int(col)
+        for i in range(5, -1, -1):
+            if self.grid[i][col] == "_":
+                self.grid[i][col] = playerLetter(player)
+                break
+
+    def win(self, player):
+        for i in range(7):
+            if self.check_col(i, player):
+                return True
+        for i in range(6):
+            if self.check_row(i, player):
+                return True
+        #diagonals
+        row = 3
+        while row <= 5:
+            for i in range(0, 4):
+                if self.check_slash(i, row):
+                    return True
+            row += 1
+        row = 3
+        while row <= 5:
+            for i in range(6, 2, -1):
+                if self.check_backslash(i, row):
+                    return True
+            row += 1
+        return False
+
+    def check_col(self, col, player):
+        x = int(col)
+        count = 1
+        for i in range(6):
+            if game.grid[i][x] == game.grid[i-1][x] == playerLetter(player):
+                count += 1
+        if count < 4:
+            return False
+        return True
+
+    def check_row(self, row, player):
+        y = int(row)
+        count = 1
+        for i in range(7):
+            if game.grid[y][i] == game.grid[y][i-1] == playerLetter(player):
+                count += 1
+            if game.grid[y][i] == playerLetter(player) != game.grid[y][i-1]:
+                count = 1
+        if count < 4:
+            return False
+        return True
+
+    def check_slash(self, col, row):
+        y = int(row) - 1
+        count = 1
+        a = int(col) + 1
+        b = a + 3
+        for i in range(a, b):
+            if game.grid[y][i] == game.grid[y+1][i-1] == playerLetter(player):
+                count += 1
+                y -= 1
+        if count < 4:
+            return False
+        return True
+
+    def check_backslash(self, col, row):
+        y = int(row) - 1
+        count = 1
+        a = int(col) - 1
+        b = a - 3
+        for i in range(a, b, -1):
+            if game.grid[y][i] == game.grid[y+1][i+1] == playerLetter(player):
+                count += 1
+                y -= 1
+        if count < 4:
+            return False
+        return True
 
 def who(turn):
     if turn % 2 == 0:
@@ -12,101 +97,21 @@ def who(turn):
 def playerLetter(player):
     return "X" if player == 1 else "O"
 
-def draw():
-    print("\n  0    1    2    3    4    5    6")
-    for row in grid:
-        print(row)
-
-def drop(col):
-    col = int(col)
-    for i in range(5, -1, -1):
-        if grid[i][col] == "_":
-            grid[i][col] = playerLetter(player)
-            break
-    
-def checkcol(col, player):
-    x = int(col)
-    count = 1
-    for i in range(6):
-        if grid[i][x] == grid[i-1][x] == playerLetter(player):
-            count += 1
-    if count < 4:
-        return False
-    return True
-
-def checkrow(row, player):
-    y = int(row)
-    count = 1
-    for i in range(7):
-        if grid[y][i] == grid[y][i-1] == playerLetter(player):
-            count += 1
-        if grid[y][i] == playerLetter(player) != grid[y][i-1]:
-            count = 1
-    if count < 4:
-        return False
-    return True
-
-def checkslash(col, row):
-    y = int(row) - 1
-    count = 1
-    a = int(col) + 1
-    b = a + 3
-    for i in range(a, b):
-        if grid[y][i] == grid[y+1][i-1] == playerLetter(player):
-            count += 1
-            y -= 1
-    if count < 4:
-        return False
-    return True
-
-def checkbackslash(col, row): #first one is (6, 3)
-    y = int(row) - 1
-    count = 1
-    a = int(col) - 1
-    b = a - 3
-    for i in range(a, b, -1):
-        if grid[y][i] == grid[y+1][i+1] == playerLetter(player):
-            count += 1
-            y -= 1
-    if count < 4:
-        return False
-    return True
-
-def win(player):
-    for i in range(7):
-        if checkcol(i, player):
-            return True
-    for i in range(6):
-        if checkrow(i, player):
-            return True
-    #diagonals
-    row = 3
-    while row <= 5:
-        for i in range(0, 4):
-            if checkslash(i, row):
-                return True
-        row += 1
-    row = 3
-    while row <= 5:
-        for i in range(6, 2, -1):
-            if checkbackslash(i, row):
-                return True
-        row += 1
-    return False
 
 import os
 
 turn = 1
 player = 1
+game = Board()
 
 while True:
     os.system("clear")
-    draw()
+    game.draw()
     player = who(turn)
     col = input("\nPlayer {}\nDrop it in what column?: ".format(player))
-    drop(col)
-    if win(player):
-        draw()
+    game.drop(col)
+    if game.win(player):
+        game.draw()
         print("\nPlayer {} wins.".format(player))
         break
     turn += 1
