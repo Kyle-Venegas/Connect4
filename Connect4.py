ea31057b5cc33cd498c4879e85047abb85c7badb
Kyle-Venegas <venegas.kyle@gmail.com>
2019-07-06 11:10:38 -0700

    added unit test and __name__guards

--- Connect4.py
+++ Connect4.py
@@ -13,11 +13,11 @@ class Board:
         for row in self.grid:
             print(row)
 
-    def drop(self, col):
+    def drop(self, col, player):
         col = int(col)
         for i in range(GRID_BOTTOM_INDEX, -1, -1):
             if self.grid[i][col] == "_":
-                self.grid[i][col] = playerLetter(player)
+                self.grid[i][col] = player_Letter(player)
                 break
 
     def win(self, player):
@@ -31,13 +31,13 @@ class Board:
         row = INITIAL_DIAGONAL_INDEX
         while row <= GRID_BOTTOM_INDEX:
             for i in range(0, 4):
-                if self.check_slash(i, row):
+                if self.check_slash(i, row, player):
                     return True
             row += 1
         row = INITIAL_DIAGONAL_INDEX
         while row <= GRID_BOTTOM_INDEX:
             for i in range(6, 2, -1):
-                if self.check_backslash(i, row):
+                if self.check_backslash(i, row, player):
                     return True
             row += 1
         return False
@@ -46,7 +46,7 @@ class Board:
         x = int(col)
         counter = 1
         for i in range(GRID_HEIGHT):
-            if game.grid[i][x] == game.grid[i-1][x] == playerLetter(player):
+            if self.grid[i][x] == self.grid[i-1][x] == player_Letter(player):
                 counter += 1
         return counter == 4
 
@@ -54,30 +54,30 @@ class Board:
         y = int(row)
         counter = 1
         for i in range(GRID_WIDTH):
-            if game.grid[y][i] == game.grid[y][i-1] == playerLetter(player):
+            if self.grid[y][i] == self.grid[y][i-1] == player_Letter(player):
                 counter += 1
-            if game.grid[y][i] == playerLetter(player) != game.grid[y][i-1]:
+            if self.grid[y][i] == player_Letter(player) != self.grid[y][i-1]:
                 counter = 1
         return counter == 4
 
-    def check_slash(self, col, row):
+    def check_slash(self, col, row, player):
         y = int(row) - 1
         counter = 1
         a = int(col) + 1
         b = a + 3
         for i in range(a, b):
-            if game.grid[y][i] == game.grid[y+1][i-1] == playerLetter(player):
+            if self.grid[y][i] == self.grid[y+1][i-1] == player_Letter(player):
                 counter += 1
                 y -= 1
         return counter == 4
 
-    def check_backslash(self, col, row):
+    def check_backslash(self, col, row, player):
         y = int(row) - 1
         counter = 1
         a = int(col) - 1
         b = a - 3
         for i in range(a, b, -1):
-            if game.grid[y][i] == game.grid[y+1][i+1] == playerLetter(player):
+            if self.grid[y][i] == self.grid[y+1][i+1] == player_Letter(player):
                 counter += 1
                 y -= 1
         return counter == 4
@@ -85,23 +85,26 @@ class Board:
 def who(turn):
     return 2 if (turn % 2 == 0) else 1
 
-def playerLetter(player):
+def player_Letter(player):
     return "X" if player == 1 else "O"
 
 import os
 
-turn = 1
-player = 1
-game = Board()
-
-while True:
-    os.system("clear")
-    game.draw()
-    player = who(turn)
-    col = input("\nPlayer {}\nDrop it in what column?: ".format(player))
-    game.drop(col)
-    if game.win(player):
+def play_Connect4():
+    turn = 1
+    player = 1
+    game = Board()
+    while True:
+        os.system("clear")
         game.draw()
-        print("\nPlayer {} wins.".format(player))
-        break
-    turn += 1
+        player = who(turn)
+        col = input("\nPlayer {}\nDrop it in what column?: ".format(player))
+        game.drop(col, player)
+        if game.win(player):
+            game.draw()
+            print("\nPlayer {} wins.".format(player))
+            break
+        turn += 1
+
+if __name__ == '__main__':
+    play_Connect4()
